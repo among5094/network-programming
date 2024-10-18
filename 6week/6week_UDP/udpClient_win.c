6f0f18c9ed12c7c744b267908a02785dc21e2e4d
among5094 <106166621+among5094@users.noreply.github.com>
2024-10-18 17:23:43 +0900

    Add files via upload

--- 6week/6week_UDP/udpClient_win.c
+++ 6week/6week_UDP/udpClient_win.c
@@ -2,66 +2,74 @@
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
+// 10월 10일 목 - CH06강 <UDP>
 
-#define BUF_SIZE 30
-void ErrorHandling(char *message);
+#define BUF_SIZE 30  // 메시지 버퍼 크기 정의
+
+void ErrorHandling(char *message);  
 
 int main(int argc, char *argv[]){
 
-    WSADATA wsaData;
-    SOCKET sock;
-    char message[BUF_SIZE];
-    int strLen;
-    int adr_sz; //추가
+    WSADATA wsaData;  // 윈도우 소켓 초기화를 위한 구조체
+    SOCKET sock;  // 소켓 핸들
+    char message[BUF_SIZE];  // 메시지 버퍼
+    int strLen;  // 수신된 메시지 길이 저장 변수
+    int adr_sz; // 추가: 수신 주소 크기를 저장할 변수
 
-    SOCKADDR_IN servAdr;
-    SOCKADDR_IN from_adr;
+    SOCKADDR_IN servAdr;  // 서버 주소 구조체
+    SOCKADDR_IN from_adr;  // 수신할 클라이언트 주소 구조체
 
-    if(argc!=3){
+    // IP 주소와 포트 번호가 입력되지 않은 경우 사용법 안내
+    if(argc != 3){
         printf("Usage: %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 
-    if(WSAStartup(MAKEWORD(2,2), &wsaData)!=0)
+    // 윈도우 소켓 라이브러리 초기화
+    if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
         ErrorHandling("WSAStartup() error!");
 
-    // UDP 소켓을 위한 SOCK_DGRAM으로 변경
-    sock=socket(PF_INET, SOCK_DGRAM, 0);
-    if(sock==INVALID_SOCKET)
+    // UDP 소켓 생성 (SOCK_DGRAM 사용)
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if(sock == INVALID_SOCKET)
         ErrorHandling("socket() error");
 
+    // 서버 주소 구조체 초기화
     memset(&servAdr, 0, sizeof(servAdr));
-    servAdr.sin_family=AF_INET;
-    servAdr.sin_addr.s_addr=inet_addr(argv[1]);
-    servAdr.sin_port=htons(atoi(argv[2]));
+    servAdr.sin_family = AF_INET;  // IPv4 사용
+    servAdr.sin_addr.s_addr = inet_addr(argv[1]);  // IP 주소 설정
+    servAdr.sin_port = htons(atoi(argv[2]));  // 포트 번호 설정
 
     while(1)
     {
-        fputs("Input message(Q to quit): ", stdout);
-        fgets(message, BUF_SIZE, stdin);
+        fputs("Input message(Q to quit): ", stdout);  // 메시지 입력 요청
+        fgets(message, BUF_SIZE, stdin);  // 메시지 입력 받기
 
+        // 'q' 또는 'Q' 입력 시 종료
         if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
             break;
 
-        if(strlen(message)!=0){
-            // sendto: UDP 패킷 전송
-            sendto(sock,message, strlen(message),0,(struct sockaddr*)&servAdr, sizeof(servAdr));
+        // 입력된 메시지가 있으면 전송
+        if(strlen(message) != 0){
+            // sendto: 서버에 UDP 패킷 전송
+            sendto(sock, message, strlen(message), 0, (struct sockaddr*)&servAdr, sizeof(servAdr));
 
-            // adr_sz 변수 초기화 (수신하는 주소 크기 설정)
+            // adr_sz 변수 초기화 (수신할 주소 크기 설정)
             adr_sz = sizeof(from_adr);
             
-            // recvfrom: UDP 패킷 수신
-            strLen=recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&from_adr, &adr_sz);
+            // recvfrom: 서버로부터 UDP 패킷 수신
+            strLen = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&from_adr, &adr_sz);
 
-            if(strLen!= -1){
-                message[strLen]=0;
-                printf("Message from server: %s", message);
+            // 수신된 메시지의 길이가 -1이 아니면 메시지 출력
+            if(strLen != -1){
+                message[strLen] = 0;  // 문자열 끝에 NULL 문자 추가
+                printf("Message from server: %s", message);  // 서버로부터 받은 메시지 출력
             }
         }
     }
 
-    closesocket(sock);
-    WSACleanup();
+    closesocket(sock);  // 소켓 닫기
+    WSACleanup();  // 윈도우 소켓 종료
     return 0;
 }
 
--- 6week/6week_UDP/udpServer_linux.c
+++ 6week/6week_UDP/udpServer_linux.c
@@ -4,58 +4,70 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-// 10.10
+// 10월 10일 목 - CH06강 <UDP>
 
-#define BUF_SIZE 30
-void error_handling(char *message);
+#define BUF_SIZE 30  // 메시지를 받을 버퍼의 크기를 정의
+
+void error_handling(char *message);  
 
 int main(int argc, char *argv[]){
 
-    int serv_sock;
-    char message[BUF_SIZE];
-    int str_len;
-    socklen_t clnt_adr_sz;
-    int connectCounter =1;
+    int serv_sock;  // 서버 소켓 파일 디스크립터
+    char message[BUF_SIZE];  // 수신된 메시지를 저장할 버퍼
+    int str_len;  // 수신된 메시지의 길이를 저장하는 변수
+    socklen_t clnt_adr_sz;  // 클라이언트 주소 구조체의 크기
+    int connectCounter = 1;  // 연결된 클라이언트 수를 추적하는 카운터
 
-    struct sockaddr_in serv_adr, clnt_adr;
-    
+    struct sockaddr_in serv_adr, clnt_adr;  // 서버와 클라이언트 주소 구조체
 
-    if(argc!=2){
+    // 프로그램 이름과 포트 번호가 제공되었는지 확인
+    if(argc != 2){
         printf("Usage: %s <port> \n", argv[0]);
         exit(1);
     }
 
-    serv_sock=socket(PF_INET, SOCK_DGRAM, 0); //
-    if(serv_sock==-1)
-        error_handling("UDP socket creation error"); // 
+    // UDP 소켓 생성을 위해 socket 함수의 두 번째 인자로 SOCK_DGRAM 전달
+    serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if(serv_sock == -1)
+        error_handling("UDP socket creation error");  // 소켓 생성 실패 시 에러 처리
 
+    // 서버 주소 구조체 초기화
     memset(&serv_adr, 0, sizeof(serv_adr));
-    serv_adr.sin_family=AF_INET;
-    serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serv_adr.sin_port=htons(atoi(argv[1]));
+    serv_adr.sin_family = AF_INET;  // IPv4 인터넷 프로토콜 사용
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);  // 모든 인터페이스에 바인딩
+    serv_adr.sin_port = htons(atoi(argv[1]));  // 포트 번호를 문자열에서 정수로 변환 후 네트워크 바이트 순서로 변환
 
-    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+    // 지정된 주소와 포트에 소켓 바인딩
+    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
         error_handling("bind() error");
 
-    // remove listen()
-
-    // add while
+    // 메시지를 처리하기 위한 무한 루프 시작
     while(1){
-        clnt_adr_sz=sizeof(clnt_adr);
-        str_len=recvfrom(serv_sock, message, BUF_SIZE, 0,
+
+        clnt_adr_sz = sizeof(clnt_adr);  // 클라이언트 주소 크기 설정
+
+        // 클라이언트로부터 메시지를 수신
+        // recvfrom 함수 호출을 통해서 데이터를 전송한 이의 주소정보도 함께 얻는데,
+        // 이 주소정보를 이용해서 수신된 데이터를 역으로 재전송
+        str_len = recvfrom(serv_sock, message, BUF_SIZE, 0,
         (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
 
-        printf("%d\n", str_len);
+        // 수신된 메시지의 길이 출력
+        printf("%d\n", str_len);  
 
-        if(str_len!=0)
-            printf("Connectd client %d \n", connectCounter++);
+        // 메시지가 수신되면 클라이언트 수를 출력하고 카운터 증가
+        if(str_len != 0)
+            printf("Connected client %d \n", connectCounter++);
 
+        // 클라이언트에게 수신된 메시지를 그대로 전송
         sendto(serv_sock, message, str_len, 0,
         (struct sockaddr*)&clnt_adr, clnt_adr_sz);
-        
     }
-    
-    close(serv_sock);
+
+
+    // 서버 소켓 닫기
+    //그러나 while문을 탈출하는 break문이 없어서 사실상 의미가 없다.
+    close(serv_sock);  
     return 0;
 }
 
