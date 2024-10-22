db685450699c24217731ac6d50f9251319de6269
among5094 <106166621+among5094@users.noreply.github.com>
2024-10-22 17:51:14 +0900

    Add files via upload

--- 5week/echo_client_win_ex.c
+++ 5week/echo_client_win_ex.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>      // exit() 함수 사용을 위한 헤더
 #include <string.h>      // 문자열 관련 함수 사용을 위한 헤더
 #include <winsock2.h>    // 윈도우 소켓 프로그래밍 관련 함수 및 구조체 정의
+// 10월 03일 온라인 수업
+// 에코 클라이언트의 문제를 보안하기 위한 코드
 
-// 버퍼 크기 정의 (서버와 주고받을 데이터의 최대 크기)
-#define BUF_SIZE 5120 // 사용자가 입력하는 버퍼 size
+
+#define BUF_SIZE 5120 // 해결법1) 버퍼 사이즈 증가
 #define BUF_RECV_SIZE 1024 // 실제 시스템 간에 데이터를 주고받을 수 있는 버퍼 크기 -> 서버에서 안 바꿨으니까
 
 // 오류 발생 시 메시지를 출력하고 프로그램 종료
@@ -12,14 +14,14 @@ void ErrorHandling(char *message);
 
 int main(int argc, char *argv[]) {
 
-    WSADATA wsaData;     // 윈도우 소켓 초기화 정보 저장
-    SOCKET hSocket;      // 클라이언트 소켓 (서버와의 연결에 사용)
+    WSADATA wsaData; // 윈도우 소켓 초기화 정보 저장
+    SOCKET hSocket; // 클라이언트 소켓 (서버와의 연결에 사용)
     char message[BUF_SIZE];  // 서버와 주고받을 데이터를 저장할 버퍼
+
     ////////////////////////////////////////////////////////////////////
-    int str_len, recv_len, recv_cnt;
-    // str_len: 송신된 메시지의 길이
-    // recv_len: 수신된 전체 데이터의 길이
-    // recv_cnt: 한 번에 받은 데이터의 길이
+    
+    int str_len, recv_len, recv_cnt; //송신된 메시지의 길이, 수신된 전체 데이터의 길이, 한 번에 받은 데이터의 길이
+   
     ////////////////////////////////////////////////////////////////////
     SOCKADDR_IN servAdr;  // 서버 주소 정보를 저장할 구조체
 
@@ -29,7 +31,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     } 
 
-    // 윈도우 소켓 라이브러리 초기화 (버전 2.2 사용)
+    // 윈도우 소켓 라이브러리 초기화
     if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
         ErrorHandling("WSAStartup() error!");
 
@@ -40,7 +42,7 @@ int main(int argc, char *argv[]) {
 
     // 서버 주소 구조체 초기화 및 설정
     memset(&servAdr, 0, sizeof(servAdr));
-    servAdr.sin_family = AF_INET;  // 주소 체계는 IPv4 사용
+    servAdr.sin_family = AF_INET;  // 주소 체계는 IPv4
     servAdr.sin_addr.s_addr = inet_addr(argv[1]);  // 입력받은 IP 주소 설정
     servAdr.sin_port = htons(atoi(argv[2]));  // 입력받은 포트 번호 설정
 
@@ -49,6 +51,7 @@ int main(int argc, char *argv[]) {
         ErrorHandling("connect() error!");
     else
         puts("Connected...........");  // 연결 성공 메시지 출력
+
     ////////////////////////////////////////////////////////////////////
 
     // 메시지 송수신 루프
@@ -65,10 +68,10 @@ int main(int argc, char *argv[]) {
         // 입력받은 메시지를 서버에 전송
         str_len = send(hSocket, message, strlen(message), 0);
 
-
+        ////////////////////////////////////////////////////////////////////
+        // 해결법2) 수신된 데이터가 전체 메시지의 길이와 같아질 때까지 반복
         recv_len = 0; // 수신된 데이터 길이를 0으로 초기화
 
-        // 서버로부터 메시지를 수신 (메시지 전체가 수신될 때까지 반복)
         while (recv_len<str_len) {
         // 아무리 메세지가 길어도 메세지를 다 받기 전까지는 화면이 넘어가지 않음!
         // 현재까지 받은 데이터의 길이(recv_len)가 보낸 데이터의 길이(str_len)보다 짧은 경우에 무한반복
@@ -82,15 +85,14 @@ int main(int argc, char *argv[]) {
             
             recv_len += recv_cnt; // 수신된 데이터의 총 길이를 갱신
         } 
-        
-        
         message[recv_len]=0; // 수신된 메시지 끝에 널 문자 추가 (문자열 종료)
+
+        ////////////////////////////////////////////////////////////////////
+
         printf("Message from server: %s", message); // 서버로부터 받은 메시지 출력
 
     }
-    ////////////////////////////////////////////////////////////////////
-
-    
+        
     closesocket(hSocket);// 클라이언트 소켓 닫기
     WSACleanup();// 윈도우 소켓 라이브러리 정리
     return 0;
