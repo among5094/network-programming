a6973880c89c0b1e81b40c00fb5c8219698af386
among5094 <106166621+among5094@users.noreply.github.com>
2024-12-01 23:21:44 +0900

    Add files via upload

--- 13week/1127_ppt04_SenderOnLinux.c
+++ 13week/1127_ppt04_SenderOnLinux.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,50 +5,66 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-/* 실습-4. 브로드캐스트 Sender 구현 on Linux */
-#define BUF_SIZE 30
+// 11월 28일 목요일
+// 실습4) 브로드캐스트 Sender 구현 on Linux
 
-void error_handling(char *message);
+#define BUF_SIZE 30 // 송신할 데이터 버퍼 크기
 
-int main(int argc, char *argv[]){
+void error_handling(char *message); // 에러 처리 함수 선언
 
-    int send_sock;
-    struct sockaddr_in mul_adr;
-    FILE *fp;
-    char buf[BUF_SIZE];
-    int so_brd=1;
+int main(int argc, char *argv[]) {
 
-    if (argc!=3){
-        printf("Usage: %s <Boradcast IP> <PORT>\n", argv[0]);
+    int send_sock; // 데이터를 송신하기 위한 소켓
+    struct sockaddr_in broad_adr; // 브로드캐스트 주소를 저장할 구조체
+    FILE *fp; // 파일 포인터 (전송할 데이터를 읽기 위해 사용)
+    char buf[BUF_SIZE]; // 전송할 데이터 버퍼
+    int so_brd = 1; // 브로드캐스트 옵션 활성화 변수
+
+    // 프로그램 실행 시 입력 인자 확인
+    if (argc != 3) {
+        printf("Usage: %s <Broadcast IP> <PORT>\n", argv[0]);
         exit(1);
     }
 
-    send_sock=socket(PF_INET, SOCK_DGRAM, 0);
-    memset(&broad_adr, 0, sizeof(broad_adr));
-    broad_adr.sin_family=AF_INET;
-    broad_adr.sin_addr.s_addr=inet_addr(argv[1]);
-    broad_adr.sin_port=htons(atoi(argv[2]));
+    // 송신 소켓 생성 (UDP 사용)
+    send_sock = socket(PF_INET, SOCK_DGRAM, 0); 
+    if (send_sock == -1) 
+        error_handling("socket() error");
 
-    setsockopt(send_sock, SOL_SOCKET,
-        SO_BROADCAST, (void*)&so_brd, sizeof(so_brd));
+    // 브로드캐스트 주소 설정
+    memset(&broad_adr, 0, sizeof(broad_adr)); // 구조체 초기화
+    broad_adr.sin_family = AF_INET; // 주소 체계 지정 (IPv4)
+    broad_adr.sin_addr.s_addr = inet_addr(argv[1]); // 브로드캐스트 IP 주소 설정
+    broad_adr.sin_port = htons(atoi(argv[2])); // 브로드캐스트 포트 번호 설정
 
-    if((fp=fopen("news.txt", "r"))==NULL)
+    // 소켓 옵션 설정: 브로드캐스트 활성화
+    if (setsockopt(send_sock, SOL_SOCKET, SO_BROADCAST, (void*)&so_brd, sizeof(so_brd)) == -1)
+        error_handling("setsockopt() error");
+
+    // 파일 열기 실패 시 에러 처리
+    if ((fp = fopen("news.txt", "r")) == NULL)
         error_handling("fopen() error");
-    
-    while(!feof(fp)) /* Broadcasting*/
-    {
-        fgets(buf, BUF_SIZE, fp);
-        sendto(send_sock, buf, strlen(buf),
-            0, (struct sockaddr*)&broad_adr, sizeof(broad_adr));
-        sleep(2);
+
+    // 파일 내용을 읽어 브로드캐스트 전송
+    while(!feof(fp)) {
+        fgets(buf, BUF_SIZE, fp); // 파일로부터 데이터를 읽음
+        if (feof(fp)) // 파일 끝에 도달하면 루프 종료
+            break;
+
+        // 데이터를 브로드캐스트 주소로 전송
+        sendto(send_sock, buf, strlen(buf), 0, (struct sockaddr*)&broad_adr, sizeof(broad_adr)); 
+        
+        sleep(2); // 2초 간격으로 데이터 전송
     }
 
-    close(send_sock);
-    return 0;
+    // 소켓 닫기 및 프로그램 종료
+    close(send_sock); // 소켓 닫기
+    return 0; // 프로그램 정상 종료
 }
 
-void error_handling(char *message){
-    fputs (message, stderr);
-    fputc('\n', stderr);
-    exit(1);
+// 에러 처리 함수
+void error_handling(char *message) {
+    fputs(message, stderr); // 에러 메시지를 표준 에러 출력
+    fputc('\n', stderr); // 개행 문자 추가
+    exit(1); // 프로그램 종료
 }
--- 13week/1127_ppt05_ReceiverOnWindows.c
+++ 13week/1127_ppt05_ReceiverOnWindows.c
@@ -1,60 +1,67 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
-#include <ws2tcpip.h> // for struct ip_mreq
-// 실습5-. 브로드캐스트 Receiver 구현 on Windows
+#include <ws2tcpip.h> // 브로드캐스트 및 멀티캐스트 관련 구조체 사용을 위한 헤더 파일
 
-#define BUF_SIZE 30
-void error_handling(char *message);
+// 11월 28일 목요일
+// 실습5) 브로드캐스트 Receiver 구현 on Windows
 
-int main(int argc, char *argv[]){
+#define BUF_SIZE 30 // 수신할 데이터의 버퍼 크기
 
-    WSADATA wsaData;
-    int recv_sock;
-    struct sockaddr_in adr;
-    int str_len;
-    char buf[BUF_SIZE];
- 
-    if (argc!=2){
-        printf("Usage: %s <PORT>\n", argv[0]);
-        exit(1);
+void error_handling(char *message); // 에러 처리 함수 선언
+
+int main(int argc, char *argv[]) {
+
+    WSADATA wsaData; // 윈속 초기화를 위한 구조체
+    int recv_sock; // 데이터를 수신하기 위한 소켓
+    struct sockaddr_in adr; // 수신 주소 정보를 저장할 구조체
+    int str_len; // 수신한 데이터의 길이를 저장할 변수
+    char buf[BUF_SIZE]; // 수신 데이터를 저장할 버퍼
+
+    // 프로그램 실행 시 입력 인자 확인
+    if (argc != 2) {
+        printf("Usage: %s <PORT>\n", argv[0]); // 사용법 안내 메시지
+        exit(1); // 입력 인자가 올바르지 않으면 프로그램 종료
     }
 
-    if (WSAStartup (MAKEWORD (2, 2), &wsaData)!=0)
+    // 윈속 초기화
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // 초기화 실패 시 에러 처리
         error_handling("WSAStartup() error!");
 
-    recv_sock=socket (PF_INET, SOCK_DGRAM, 0);
+    // 수신 소켓 생성 (UDP 사용)
+    recv_sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (recv_sock == INVALID_SOCKET) // 소켓 생성 실패 시 에러 처리
+        error_handling("socket() error");
 
-    memset(&adr, 0, sizeof(adr));
-    adr.sin_family=AF_INET;
-    adr.sin_addr.s_addr=htonl(INADDR_ANY);
-    adr.sin_port=htons(atoi(argv[1]));
+    // 소켓 주소 설정
+    memset(&adr, 0, sizeof(adr)); // 구조체 초기화
+    adr.sin_family = AF_INET; // 주소 체계 지정 (IPv4)
+    adr.sin_addr.s_addr = htonl(INADDR_ANY); // 모든 네트워크 인터페이스에서 수신
+    adr.sin_port = htons(atoi(argv[1])); // 수신할 포트 번호 설정
 
-    if (bind(recv_sock, (SOCKADDR*)&adr, sizeof(adr))==-1)
+    // 소켓에 주소 바인딩
+    if (bind(recv_sock, (SOCKADDR*)&adr, sizeof(adr)) == SOCKET_ERROR) // 바인딩 실패 시 에러 처리
         error_handling("bind() error");
 
-    
-    while(1)
-    {
-        str_len=recvfrom (recv_sock, buf, BUF_SIZE-1, 0, NULL, 0);
-        if(str_len<0)
+    // 데이터 수신 루프
+    while (1) {
+        str_len = recvfrom(recv_sock, buf, BUF_SIZE - 1, 0, NULL, 0); // 데이터 수신
+        if (str_len < 0)
             break;
-        buf[str_len]=0;
-        fputs(buf, stdout);
+        buf[str_len] = 0; // 문자열 끝에 NULL 추가
+        fputs(buf, stdout); // 수신된 데이터 출력
     }
-    
-    closesocket(recv_sock);
-    WSACleanup();
-    return 0;
 
+    // 소켓 닫기 및 윈속 종료
+    closesocket(recv_sock); // 소켓 닫기
+    WSACleanup(); // 윈속 정리
+    return 0; // 프로그램 정상 종료
 }
 
-void error_handling(char *message)
-{
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
-
+// 에러 처리 함수
+void error_handling(char *message) {
+    fputs(message, stderr); // 에러 메시지를 표준 에러 출력
+    fputc('\n', stderr); // 개행 문자 추가
+    exit(1); // 프로그램 종료
 }
